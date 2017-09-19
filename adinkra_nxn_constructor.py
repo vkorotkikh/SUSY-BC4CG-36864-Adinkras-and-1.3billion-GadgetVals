b8d486db2de6fad5d631181afd05931d2d4f8772
vkorotkikh <va.korotki@gmail.com>
2017-09-18 20:31:40 -0400

    Update

--- adinkra_nxn_constructor.py
+++ adinkra_nxn_constructor.py
@@ -1,12 +1,17 @@
 # ******************************************************************************
-# Name:    Faster way of computing all BC4 Adinkras
+# Name:    Adinkra NxN Constructor
 # Author:  Vadim Korotkikh
 # Email:   va.korotki@gmail.com
+# Date:	   February 2017
+#
+# Description: Algorithm for generating n-colour sized Adinkras of n-node
+# matrix representations
 
-import sys, time itertools
-import numpy as np
 from numpy.linalg import inv
-
+import numpy as np
+import itertools
+import time
+import sys
 
 # Makes colums/rows that compose an Identity matrix
 def unit_vector(n,i):
@@ -29,13 +34,11 @@ def gen_permutations(n):
 
 # Generate all sign permutations of an nxn Identity Matrix
 def gen_signm(n):
-	items	= [1] * int(n)
-	# itemsl = np.ones(n)
-	# items = itemsl.tolist()
-    # n   = int(n)
-    # items = [1] * n
-	sign_mat = []
-	for signs in itertools.product([-1,1], repeat=n):
+
+	n 			= int(n)
+	items		= [1] * n
+	sign_mat 	= []
+	for signs in itertools.product([-1,1], repeat=len(items)):
 		temp = np.array([a*sign for a,sign in zip(items,signs)],dtype=int)
 		# ptemp.append(temp)
 		sign_mat.append(np.diag(temp))
@@ -55,13 +58,13 @@ def gen_product_matrices(n):
 			legal_matrices.append(np.dot(x,y))
 	return legal_matrices
 
-# ****************
+# ********************************
 def pairing(matli, matlj):
 	ri = np.transpose(matli)
 	rj = np.transpose(matlj)
-	# sig_lj =  np.multiply(matlj, -1)
 	# tmat = np.dot(matli,rj) + np.dot(matlj,ri)
 	rtmat = np.dot(ri,matlj) + np.dot(rj,matli)
+
 	# if np.array_equal(ri, inv(matli)) and np.array_equal(rj, inv(matlj)):
 	return (np.count_nonzero(rtmat) == 0)
 
@@ -75,21 +78,23 @@ def make_adinkras(k, legal_matrices):
 	"""
 	# main_list	= [[] for i in range(len(legal_matrices))]
 	# print(len(main_list))
-	""" Define common used lists and their size for speed. """
+	""" Preallocate lists """
 	xtest_pack	= [None] * 12
 	fourpack	= [None] * 4
 
 	if k == 1:
 		return [list(l) for l in legal_matrices]
 	else:
-		# adinkra_list = [None] * 36864
-		adinkra_list	= []
+		adinkra_list = []
+		print("Length lmats", len(legal_matrices))
 
 		for i, mat in enumerate(legal_matrices):			# Find all matrix pairs for mat
 			# good_mats = [m for m in legal_matrices if pairing(mat,m)]
+
 			# test_mats = [ind[0] for ind in enumerate(legal_matrices) if pairing(mat, ind[1])]
 			xtest_pack = [ind for ind in enumerate(legal_matrices) if pairing(mat, ind[1])]
 			# main_list[i] = xtest_pack
+
 			for val in xtest_pack:
 				# main_list[val[0]] = [(i,mat)]
 				fourpack	= [nmat for nmat in xtest_pack if pairing(val[1], nmat[1])]
@@ -97,20 +102,33 @@ def make_adinkras(k, legal_matrices):
 				for xval in fourpack:
 					for lastx in [nmat for nmat in fourpack if pairing(xval[1], nmat[1])]:
 						# temp = [(i,mat), val, xval, lastx]
+						# temp = [i, val[0], xval[0], lastx[0]]
 						adinkra_list.append([(i,mat), val, xval, lastx])
+			# for im in good_mats:
+			# for m in good_mats:
+			# 	good_mats_redux = [mx for mx in good_mats if pairing(m,mx)]
+			# 	print("Redux", len(good_mats_redux))
+			# 	for rm in good_mats_redux:
+			# 		flist = [rx for rx in good_mats_redux if pairing(rm, rx)]
+			# 		print("flist:", len(flist))
+			# print(len(good_mats))
+			# biglist  += [[mat]+mlist for mlist in make_adinkras(k-1, good_mats)]
 		return adinkra_list
 
 
-# ****************
-def makeall_adinkras(k):
+# ********************************
+def makeall_adinkras(k,n):
 
-	main_tetrad = make_adinkras(k, gen_product_matrices(k))
-	print(len(main_tetrad))
+	# print(len(test_list))
+	# return make_matrices(n, gen_product_matrices(n))
+	main_tetrad = make_adinkras(k,gen_product_matrices(n))
 
-# ****************
+	return main_tetrad
+
+# ********************************
 # Run main()
-start_time = time.time()
-
-makeall_adinkras(4)
-print("-- Execution time --")
-print("---- %s seconds ----" % (time.time() - start_time))
+# start_time = time.time()
+#
+# makeall_adinkras(4,4)
+# print("-- Execution time --")
+# print("---- %s seconds ----" % (time.time() - start_time))
